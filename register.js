b8fa26bc85f892a8d381fb9038437277a223f5c3
Afia <124785215+AlizaKhan3@users.noreply.github.com>
2024-10-07 23:19:36 +0500

    Update register.js

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-import { auth, createUserWithEmailAndPassword, RecaptchaVerifier, signInWithPhoneNumber, googleProvider, signInWithPopup, GoogleAuthProvider } from "./firebase.js";
+import { auth, createUserWithEmailAndPassword, RecaptchaVerifier, signInWithPhoneNumber, googleProvider, signInWithPopup, GoogleAuthProvider, facebookProvider, FacebookAuthProvider } from "./firebase.js";
 
 // window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
 //     'size': 'invisible',
@@ -29,28 +29,28 @@ const register = () => {
             console.log("error", errorMessage);
             // Swal.fire(`${errorMessage}`);
         });
-
-    //sign in with Phone Number -- requires billing
-    // const phone = document.getElementById("phone");
-    // window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
-    // const appVerifier = window.recaptchaVerifier;
-    // console.log("phone Number", phone.value);
-
-    // signInWithPhoneNumber(auth, `+${phone.value}`, appVerifier)
-    //     .then((confirmationResult) => {
-    //         // SMS sent. Prompt user to type the code from the message, then sign the
-    //         // user in with confirmationResult.confirm(code).
-    //         window.confirmationResult = confirmationResult;
-    //         console.log("SMS sent! ", confirmationResult);
-    //     }).catch((error) => {
-    //         // Error; SMS not sent
-    //         console.log("SMS not sent ", error)
-    //     });
 }
 
-// let signInButton = document.getElementById("sign-in-button");
-// signInButton.addEventListener('click', register);
+ //---------------sign in with Phone Number - requires billing------------------
+//  let signinWithPhone = () => {
+//     const phone = document.getElementById("phone");
+//     window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
+//     const appVerifier = window.recaptchaVerifier;
+//     console.log("phone Number", phone.value);
 
+//     signInWithPhoneNumber(auth, `+${phone.value}`, appVerifier)
+//         .then((confirmationResult) => {
+//             // SMS sent. Prompt user to type the code from the message, then sign the
+//             // user in with confirmationResult.confirm(code).
+//             window.confirmationResult = confirmationResult;
+//             console.log("SMS sent! ", confirmationResult);
+//         }).catch((error) => {
+//             // Error; SMS not sent
+//             console.log("SMS not sent ", error)
+//         });
+//  }
+
+//-------------------Google Signin-----------------------------
 let googleAuthentication = () => {
     signInWithPopup(auth, googleProvider)
     .then((result) => {
@@ -68,8 +68,34 @@ let googleAuthentication = () => {
     });
 }
 
+//-------------------Facebook Signin-----------------------------
+let facebookAuthentication = () => {
+    // const auth = getAuth();
+    signInWithPopup(auth, facebookProvider)
+      .then((result) => {
+        const user = result.user;
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+        console.log("Facebook User Signin--> ", user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = FacebookAuthProvider.credentialFromError(error);
+        console.log("Can't Signup using Facebook Auth until verified");
+      });
+}
+
+
 let registerUser = document.getElementById("registerUser");
 registerUser.addEventListener('click', register);
 
+// let signInButton = document.getElementById("sign-in-button");
+// signInButton.addEventListener('click', signinWithPhone);
+
 let signinWithGoogle = document.getElementById("signin-with-google");
 signinWithGoogle.addEventListener('click', googleAuthentication);
+
+let signinWithFacebook = document.getElementById("signin-with-facebook");
+signinWithFacebook.addEventListener('click', facebookAuthentication);
